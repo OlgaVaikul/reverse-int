11a42ca561c52d22cea0a24ac07226945740cec0
Olga Vaikul <olenka1201@list.ru>
2023-01-24 14:07:26 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function reverseInt (n) {
-    n = n + "";
-    return Number(String(n).split("").reverse().join(""));
+    return String(Math.abs(n)).split("").reverse().join("");
   }
 
 
